0b934804f6c236e06f1ec3fe9cc8b4dcdd3cf3ac
Nick W <nickwindt@hotmail.nl>
2019-05-26 21:59:17 +0200

    Browser Extensions + ASP.NET MVC

---
+++ DemoApp/BrowserExtension/background.js
@@ -0,0 +1,14 @@
+﻿"use strict";
+
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        if (request.target === "Red") {
+            chrome.tabs.query({}, //{  currentWindow: true},
+                function (tabs) {
+                    for (let tab of tabs) {
+                        chrome.tabs.sendMessage(tab.id, { target: "setColor" });
+                    }
+                });
+        }
+        sendResponse("Enjoy colors <3");
+    });
---
+++ DemoApp/BrowserExtension/content.js
@@ -0,0 +1,31 @@
+﻿function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+// Add logic to only do this when received by the web application
+chrome.runtime.sendMessage({ target: "Red" }, function (response) {
+    console.log(response);
+});
+
+
+chrome.runtime.onMessage.addListener(request => {
+    if (request.target === "setColor") {
+        window.document.body.style.backgroundColor = getRandomColor();
+
+        for (var button of window.document.getElementsByTagName("button")) {
+            console.log(button);
+            button.style.background = getRandomColor();
+        }
+
+        for (var div of window.document.getElementsByTagName("*")) {
+            div.style.backgroundColor = getRandomColor();
+        }
+
+        return Promise.resolve({ response: "Hi from content script" });
+    }
+});
